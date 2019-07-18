d0d2d97e2a1556b80f3ae16ab19a65f36092951e
patrick <patrick.welzel1996@googlemail.com>
2019-07-18 22:46:11 +0200

    PLayable without Computer

--- UNOGame.js
+++ UNOGame.js
@@ -1,9 +1,17 @@
+var amountOfCards = 5;
 var colors = [
     "red",
     "green",
     "yellow",
     "blue",
 ];
+var gameState;
+(function (gameState) {
+    gameState[gameState["PlayersDraw"] = 0] = "PlayersDraw";
+    gameState[gameState["ComputersDraw"] = 1] = "ComputersDraw";
+    gameState[gameState["Won"] = 2] = "Won";
+    gameState[gameState["Lost"] = 3] = "Lost";
+})(gameState || (gameState = {}));
 var Card = /** @class */ (function () {
     function Card(value, color) {
         this.value = value;
@@ -13,60 +21,153 @@ var Card = /** @class */ (function () {
 }());
 var cardStack = /** @class */ (function () {
     function cardStack() {
-        this.myStack = this.fillStack();
+        this.myStack = new Array();
+        this.fillStack();
+        this.shuffleCards();
     }
     cardStack.prototype.fillStack = function () {
         var nCard;
-        var arr = new Array();
         for (var i = 0; i < colors.length; i++) {
-            console.log("Color: " + colors[i]);
             for (var cardValue = 1; cardValue <= 9; cardValue++) {
                 nCard = new Card(cardValue.toString(), colors[i]);
-                console.log(nCard);
-                arr.push(nCard);
+                this.myStack.push(nCard);
             }
         }
-        console.log(arr);
-        return arr;
+    };
+    cardStack.prototype.shuffleCards = function () {
+        var rand, tmp;
+        for (var i = 0; i < this.myStack.length; i++) {
+            rand = Math.floor(Math.random() * this.myStack.length);
+            tmp = this.myStack[i];
+            this.myStack[i] = this.myStack[rand];
+            this.myStack[rand] = tmp;
+        }
+    };
+    cardStack.prototype.getCard = function () {
+        return this.myStack.pop();
     };
     return cardStack;
+}());
+var cardPile = /** @class */ (function () {
+    function cardPile(card) {
+        this.myPile = new Array();
+        this.myPile.push(card);
+    }
+    cardPile.prototype.getTopCard = function () {
+        var l = this.myPile.length - 1;
+        return new Card(this.myPile[l].value, this.myPile[l].color);
+    };
+    cardPile.prototype.addCard = function (newCard) {
+        this.myPile.push(newCard);
+    };
+    cardPile.prototype.checkTurn = function (card) {
+        var topCard = this.getTopCard();
+        if (topCard.color == card.color || topCard.value == card.value) {
+            return (true);
+        }
+        else {
+            return (false);
+        }
+    };
+    return cardPile;
 }());
 var player = /** @class */ (function () {
     function player(name) {
         this.name = name;
+        this.hand = new Array();
     }
-    player.prototype.newHand = function () {
+    player.prototype.addCard = function (newCard) {
+        this.hand.push(newCard);
+    };
+    player.prototype.removeCard = function (card) {
+        console.log(this.hand);
+        console.log(card);
+        for (var i = 0; i < this.hand.length; i++) {
+            console.log(this.hand[i]);
+            if (this.hand[i].value == card.value && this.hand[i].color == card.color) {
+                console.log("found");
+                this.hand.splice(i, 1);
+            }
+        }
+        console.log(this.hand);
     };
     player.prototype.getName = function () {
-        return "Hello" + this.name;
+        return this.name;
     };
     return player;
 }());
-var user = new player("Lisa");
-var stack = new cardStack();
-console.log(document);
-document.getElementById("button").innerHTML = user.getName();
-var Dynamically_create_element = /** @class */ (function () {
-    function Dynamically_create_element() {
+function giveOutCards(players, stack) {
+    //console.log(stack);
+    for (var i = 1; i <= amountOfCards; i++) {
+        for (var j = 0; j < players.length; j++) {
+            players[j].addCard(stack.getCard());
+        }
     }
-    Dynamically_create_element.prototype.Create_Element = function (htmlelent) {
-        var element = document.createElement("input");
-        //Assign different attributes to the element.
-        element.setAttribute("type", htmlelent);
-        element.setAttribute("value", htmlelent);
-        element.setAttribute("name", htmlelent);
-        element.setAttribute("style", "color:Red");
-        document.body.appendChild(element);
-    };
-    return Dynamically_create_element;
-}());
-window.onload = function () {
-    var button = document.createElement('button');
-    button.innerText = "Add";
-    button.onclick = function () {
-        var doc = document.getElementById('Select1').value;
-        var create = new Dynamically_create_element();
-        create.Create_Element(doc);
-    };
-    document.body.appendChild(button);
-};
+}
+function printPlayerStack(player) {
+    var html = "";
+    var card;
+    for (var i = 0; i < player.hand.length; i++) {
+        card = player.hand[i];
+        html += "<td><button onClick=\"cardClicked('" + card.color + "', '" + card.value + "')\" type=\"" + card.color + "\">" + card.value + "</button></td>";
+    }
+    document.getElementById("playerstack").innerHTML = html;
+}
+function printComputerStack(player) {
+    var html = "";
+    var card;
+    for (var i = 0; i < player.hand.length; i++) {
+        card = player.hand[i];
+        html += "<td><button type='backside'>X</button></td>";
+    }
+    document.getElementById("computerstack").innerHTML = html;
+}
+function printPile(pile) {
+    var card = pile.myPile[pile.myPile.length - 1];
+    var html = "<button type='" + card.color + "'>" + card.value + "</button>";
+    document.getElementById("pile_card").innerHTML = html;
+}
+function printStack(stack) {
+    var html = "<button onClick='takeCard()' type='backside'>X</button>";
+    if (stack.myStack.length > 0) {
+        document.getElementById("stack").innerHTML = html;
+    }
+    else {
+        document.getElementById("stack").innerHTML = "";
+    }
+}
+/* Button Functions */
+function cardClicked(color, value) {
+    //if (state == gameState.PlayersDraw) {
+    //alert("Click! "+color+" "+value);
+    var clCard = new Card(value, color);
+    if (pile.checkTurn(clCard)) {
+        user.removeCard(clCard);
+        pile.addCard(clCard);
+        state = gameState.ComputersDraw;
+        printPlayerStack(user);
+        printPile(pile);
+    }
+    else {
+        alert("You cant place this card");
+    }
+    //} else {
+    //	alert("It's not your turn!");
+    //}
+}
+function takeCard() {
+    user.addCard(stack.getCard());
+    printPlayerStack(user);
+    state = gameState.ComputersDraw;
+}
+/* Initialize */
+var user = new player("Lisa");
+var comp = new player("Computer");
+var stack = new cardStack();
+var pile = new cardPile(stack.getCard());
+giveOutCards([user, comp], stack);
+printPlayerStack(user);
+printComputerStack(comp);
+printPile(pile);
+printStack(stack);
+var state = gameState.PlayersDraw;
--- UNOGame.ts
+++ UNOGame.ts
@@ -1,9 +1,18 @@
+let amountOfCards: number = 5;
+
 let colors: string[] = [
 		"red", 
 		"green", 
 		"yellow", 
 		"blue",
 	];
+	
+enum gameState {
+	PlayersDraw,
+	ComputersDraw,
+	Won,
+	Lost,
+}
 
 class Card {
 	value: string;
@@ -16,25 +25,63 @@ class Card {
 }
 
 class cardStack {
-	myStack: Card[];
+	myStack: Card[] = new Array();
 	
 	constructor(){
-		this.myStack = this.fillStack();
+
+		this.fillStack();
+		this.shuffleCards();
 	}
 	
-	fillStack():Card[] {
+	fillStack() {
 		let nCard;
-		let arr = new Array();
 		for(let i=0; i< colors.length; i++){
-			console.log("Color: "+ colors[i]);
 			for (let cardValue = 1; cardValue <=9; cardValue++){
 				nCard = new Card(cardValue.toString(), colors[i]);
-				console.log(nCard);
-				arr.push(nCard);
+				this.myStack.push(nCard);
 			}
 		}
-		console.log(arr);
-		return arr;
+	}
+	
+	shuffleCards(){
+		let rand, tmp;
+		for(let i=0; i<this.myStack.length; i++){
+			rand = Math.floor(Math.random()*this.myStack.length);
+			tmp = this.myStack[i];
+			this.myStack[i]=this.myStack[rand];
+			this.myStack[rand]=tmp;
+		}
+	}
+	
+	getCard(){
+		return this.myStack.pop();
+	}
+}
+
+class cardPile {
+	myPile: Card[] = new Array();
+	
+	constructor(card: Card){
+		this.myPile.push(card);
+	}
+	
+	private getTopCard():Card {
+		let l:number = this.myPile.length-1
+		return new Card(this.myPile[l].value, this.myPile[l].color);
+	}
+	
+	addCard(newCard: Card){
+		this.myPile.push(newCard);
+	}
+	
+	checkTurn(card: Card):boolean {
+		let topCard: Card = this.getTopCard();
+
+		if (topCard.color == card.color || topCard.value == card.value){
+			return(true)
+		} else {
+			return(false);
+		}
 	}
 }
 
@@ -44,21 +91,121 @@ class player {
 	
 	constructor(name: string){
 		this.name = name;
+		this.hand = new Array();
 	}
 	
-	newHand(){
-		
+	addCard(newCard: Card){
+		this.hand.push(newCard);
+	}
+	
+	removeCard(card: Card){
+		console.log(this.hand);
+		console.log(card)
+		for (let i=0;i<this.hand.length; i++){
+			console.log(this.hand[i]);
+			
+			if(this.hand[i].value == card.value && this.hand[i].color == card.color){
+				console.log("found")
+				this.hand.splice(i, 1);
+			}
+		}
+		console.log(this.hand);
 	}
 	
 	getName(){
-		return "Hello"+this.name;
+		return this.name;
 	}
 }
 
-var user = new player("Lisa");
-var stack = new cardStack();
-console.log(document);
-document.getElementById("button").innerHTML = user.getName();
+
+
+function giveOutCards(players: player[], stack: cardStack){
+	//console.log(stack);
+	for(let i=1; i<=amountOfCards;i++){
+		for (let j=0; j<players.length; j++){
+			players[j].addCard(stack.getCard());
+		} 
+	}
+}
+
+function printPlayerStack(player: player){
+	let html: string = "";
+	let card: Card;
+	for (let i=0; i<player.hand.length; i++){
+		card = player.hand[i];
+		html+=`<td><button onClick="cardClicked('${card.color}', '${card.value}')" type="${card.color}">${card.value}</button></td>`;
+	}
+	document.getElementById("playerstack").innerHTML = html;
+}
+
+function printComputerStack(player: player){
+	let html: string = "";
+	let card: Card;
+	for (let i=0; i<player.hand.length; i++){
+		card = player.hand[i];
+		html+="<td><button type='backside'>X</button></td>"
+	}
+	document.getElementById("computerstack").innerHTML = html;
+}
+
+function printPile(pile: cardPile){
+	let card: Card = pile.myPile[pile.myPile.length-1];
+	let html: string = `<button type='${card.color}'>${card.value}</button>`;
+	
+	document.getElementById("pile_card").innerHTML = html;
+}
+
+function printStack(stack: cardStack){
+	let html: string = "<button onClick='takeCard()' type='backside'>X</button>";
+	if (stack.myStack.length > 0) {
+		document.getElementById("stack").innerHTML = html;
+	} else {
+		document.getElementById("stack").innerHTML = "";
+	}
+}
+
+/* Button Functions */
+function cardClicked(color: string, value: string){
+	//if (state == gameState.PlayersDraw) {
+		//alert("Click! "+color+" "+value);
+		let clCard=new Card(value, color);
+		if(pile.checkTurn(clCard)){
+			user.removeCard(clCard);
+			pile.addCard(clCard);
+			state = gameState.ComputersDraw;
+			printPlayerStack(user);
+			printPile(pile);
+		} else {
+			alert("You cant place this card");
+		}
+	//} else {
+	//	alert("It's not your turn!");
+	//}
+	
+}
+
+function takeCard(){
+	user.addCard(stack.getCard());
+	printPlayerStack(user);
+	printStack(stack);
+	state = gameState.ComputersDraw;
+}
+
+/* Initialize */
+let user = new player("Lisa");
+let comp = new player("Computer");
+let stack: cardStack = new cardStack();
+let pile: cardPile = new cardPile(stack.getCard());
+
+giveOutCards([user, comp], stack);
+printPlayerStack(user);
+printComputerStack(comp);
+printPile(pile);
+printStack(stack);
+
+let state = gameState.PlayersDraw;
+
+
 
 
 
